dfad2662005accc7e081018241ad66f3ad021072
binhex <megalith01@gmail.com>
2017-10-24 17:16:10 +0100

    initial commit

---
+++ build/root/install.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+# exit script if return code != 0
+set -e
+
+# build scripts
+####
+
+# download build scripts from github
+curly.sh -rc 6 -rw 10 -of /tmp/scripts-master.zip -url https://github.com/binhex/scripts/archive/master.zip
+
+# unzip build scripts
+unzip /tmp/scripts-master.zip -d /tmp
+
+# move shell scripts to /root
+mv /tmp/scripts-master/shell/arch/docker/*.sh /root/
+
+# pacman packages
+####
+
+# define pacman packages
+pacman_packages=""
+
+# install compiled packages using pacman
+if [[ ! -z "${pacman_packages}" ]]; then
+	pacman -S --needed $pacman_packages --noconfirm
+fi
+
+# aor packages
+####
+
+# define arch official repo (aor) packages
+aor_packages="libreoffice-fresh"
+
+# call aor script (arch official repo)
+source /root/aor.sh
+
+# aur packages
+####
+
+# define aur packages
+aur_packages=""
+
+# call aur install script (arch user repo)
+source /root/aur.sh
+
+# config libreoffice
+####
+
+cat <<'EOF' > /tmp/startcmd_heredoc
+# run libreoffice
+/usr/bin/libreoffice
+EOF
+
+# replace startcmd placeholder string with contents of file (here doc)
+sed -i '/# STARTCMD_PLACEHOLDER/{
+    s/# STARTCMD_PLACEHOLDER//g
+    r /tmp/startcmd_heredoc
+}' /home/nobody/start.sh
+rm /tmp/startcmd_heredoc
+
+# config novnc
+###
+
+# overwrite novnc favicon with application favicon
+cp /home/nobody/favicon.ico /usr/share/novnc/
+
+# config openbox
+####
+
+cat <<'EOF' > /tmp/menu_heredoc
+    <item label="LibreOffice Fresh">
+    <action name="Execute">
+      <command>libreoffice</command>
+      <startupnotify>
+        <enabled>yes</enabled>
+      </startupnotify>
+    </action>
+    </item>
+EOF
+
+# replace menu placeholder string with contents of file (here doc)
+sed -i '/<!-- APPLICATIONS_PLACEHOLDER -->/{
+    s/<!-- APPLICATIONS_PLACEHOLDER -->//g
+    r /tmp/menu_heredoc
+}' /home/nobody/.config/openbox/menu.xml
+rm /tmp/menu_heredoc
+
+# container perms
+####
+
+# create file with contets of here doc
+cat <<'EOF' > /tmp/permissions_heredoc
+echo "[info] Setting permissions on files/folders inside container..." | ts '%Y-%m-%d %H:%M:%.S'
+
+chown -R "${PUID}":"${PGID}" /tmp /usr/share/themes /home/nobody /usr/share/novnc/usr/include/libreoffice /usr/lib/libreoffice /usr/share/doc/libreoffice /usr/share/libreoffice /usr/share/idl/libreoffice /usr/share/applications/ /etc/xdg
+chmod -R 775 /tmp /usr/share/themes /home/nobody /usr/share/novnc/usr/include/libreoffice /usr/lib/libreoffice /usr/share/doc/libreoffice /usr/share/libreoffice /usr/share/idl/libreoffice /usr/share/applications/ /etc/xdg
+
+EOF
+
+# replace permissions placeholder string with contents of file (here doc)
+sed -i '/# PERMISSIONS_PLACEHOLDER/{
+    s/# PERMISSIONS_PLACEHOLDER//g
+    r /tmp/permissions_heredoc
+}' /root/init.sh
+rm /tmp/permissions_heredoc
+
+# env vars
+####
+
+# cleanup
+yes|pacman -Scc
+rm -rf /usr/share/locale/*
+rm -rf /usr/share/man/*
+rm -rf /usr/share/gtk-doc/*
+rm -rf /tmp/*
